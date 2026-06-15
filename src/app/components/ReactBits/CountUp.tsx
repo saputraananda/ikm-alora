e544e3402497f8a5bdbfa44d2c99d5a2dca79976
Marsya <marsyahalya117@gmail.com>
2026-06-15 10:17:40 +0700

    fix error deployment

--- src/app/components/ReactBits/CountUp.tsx
+++ src/app/components/ReactBits/CountUp.tsx
@@ -39,7 +39,7 @@ export default function CountUp({
   useEffect(() => {
     springValue.on("change", (latest) => {
       if (ref.current) {
-        ref.current.textContent = Intl.NumberFormat("en-US").format(latest.toFixed(0));
+        ref.current.textContent = Intl.NumberFormat("en-US").format(Math.round(latest));
       }
     });
   }, [springValue]);
